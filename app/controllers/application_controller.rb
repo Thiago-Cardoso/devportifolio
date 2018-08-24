10e43dc04d0631ed5781440d5bd6b6c5176c2284
Thiago-Cardoso <trovador182@gmail.com>
2018-08-23 23:01:06 -0300

    customize devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,8 @@ class ApplicationController < ActionController::Base
           format.html { redirect_to root_path, flash: { error: 'Unauthorized' } }
         end
       end
+
+      #def after_sign_in_path_for(resource)
+        #portfolios_path
+      #end
 end
