7ae1a91dfc07e699eb042662d0a237aec8c7f19a
Thiago-Cardoso <trovador182@gmail.com>
2018-08-26 21:39:03 -0300

    test create new portfolio with capybara

--- features/step_definitions/portfolio_steps.rb
+++ features/step_definitions/portfolio_steps.rb
@@ -21,3 +21,22 @@ Dado "possui {int} portfólio(s) cadastrado(s)" do |count|
   Então "volta para a página de login" do
     expect(page).to have_current_path("/users/sign_in")
   end
+
+  Quando "clica para criar um novo portfólio" do
+    find(:xpath, "//a[@class='new-portfolio']").click
+  end
+  
+  
+  Então "o usuário deve ter {int} portfólios cadastrados" do |count|
+    expect(@user.portfolios.count).to eq count
+  end
+  
+  
+  Então "deve ver os dados iniciais do portfólio" do
+    expect(page).to have_content(@user.portfolios.last.slug)
+  end
+  
+  
+  Então "é direcionado para a pagina de edição do portfólio" do
+    expect(page).to have_current_path(edit_portfolio_path(@user.portfolios.last))
+  end
