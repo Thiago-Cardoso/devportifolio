14d8daa835777cee60aec40c6d6e0662bfb980e7
Thiago-Cardoso <trovador182@gmail.com>
2018-09-20 17:05:03 -0300

    created controller add_block

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :portfolios, except: :new do
     resources :tags, only: [:create, :destroy], param: :tag_id, controller: :portfolio_tags
+    resources :blocks, only: [:index, :create, :destroy]
   end
   resources :tags, only: :index
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
