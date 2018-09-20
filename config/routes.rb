733747fb93f72ce8a71db0e26b5dd629504fe16e
Thiago-Cardoso <trovador182@gmail.com>
2018-09-20 14:27:26 -0300

    create tag controller and policies for tag

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'pages#home'
   devise_for :users
-  resources :portfolios, except: :new
+  resources :portfolios, except: :new do
+    resources :tags, only: [:create, :destroy], param: :tag_id, controller: :portfolio_tags
+  end
+  resources :tags, only: :index
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
