386c55fc2589fc6174db519d91a05d5e79883013
Thiago-Cardoso <trovador182@gmail.com>
2018-09-21 14:09:33 -0300

    created block experience

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     resources :tags, only: [:create, :destroy], param: :tag_id, controller: :portfolio_tags
     resources :blocks, only: [:index, :create, :destroy]
     resources :profiles, only: [:index, :update]
+    resources :experiences, only: [:index, :create, :destroy]
   end
   resources :tags, only: :index
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
