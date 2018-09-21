794b46ec45db1e6986e5130e101cf42d0eef6470
Thiago-Cardoso <trovador182@gmail.com>
2018-09-21 15:09:47 -0300

    created component of block skills

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
     resources :profiles, only: [:index, :update]
     resources :experiences, only: [:index, :create, :destroy]
     resources :educations, only: [:index, :create, :destroy]
+    resources :skills, only: [:index, :create, :destroy]
   end
   resources :tags, only: :index
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
